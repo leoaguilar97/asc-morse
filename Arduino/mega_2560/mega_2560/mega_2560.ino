4a8e82969a2957aab2e73a0637cba30af13f3750
Leo Aguilar <leoaguilarsanchez97@gmail.com>
2020-02-11 12:39:24 -0600

    Update mega_2560.ino

--- Arduino/mega_2560/mega_2560/mega_2560.ino
+++ Arduino/mega_2560/mega_2560/mega_2560.ino
@@ -1,9 +1,19 @@
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
+  Serial1.begin(115200);
 
+  Serial.println(">> Inicio controlador mega_2560");
+
+  
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (Serial1.available() > 0){ 
+    String val = Serial1.readString();
 
+    Serial.println(String("Recibido: ") + val);
+
+    delay(100);
+  }
 }
