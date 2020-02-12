c816179956adb2ddb555aed899880b824c2948bc
edaral3 <arnolso201@gmail.com>
2020-02-12 15:32:04 -0600

    Metodos publicos comentados

--- Arduino/buzzer/buzzer.h
+++ Arduino/buzzer/buzzer.h
@@ -5,18 +5,17 @@
 class Buzzer
 {
   public:
-    Buzzer(int pin1,int pin2);
-    void punto();
-    void raya();
-    void reproducirString(String cadena);
-    void settiempoPausaSonido(int tiempo);
-    void settiempoPausaLetra(int tiempo);
-    
-    void ok();
-    void fail();
-    
+    Buzzer(int pin1,int pin2); //pin1 = sonido y pin2 = encender y apagar buzer.
+    void punto(); //Sonido de punto.
+    void raya(); //Sonido de raya.
+    void reproducirString(String cadena); //Seproduce texto en morse.
+    void settiempoPausaSonido(int tiempo); //Cambial el tiempo de la pausa entre puntos o rayas. Tiempo por defecto de 400 milisegundos.
+    void settiempoPausaLetra(int tiempo); //Cambial el tiempo de la pausa entre letras. Tiempo por defecto de 1500 milisegundos.
+    void ok(); //Sonido de ok.
+    void fail(); //Sonido de falla.
+
   private:
-    void sonidosMorse(int tiempo,int sonido);
+    void sonidosMorse(int tiempo,int sonido); 
     void pausa(int tiempo);
     void pausaLetra();
     void pausaSonido();
